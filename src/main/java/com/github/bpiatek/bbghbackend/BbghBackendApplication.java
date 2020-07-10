f6613c025e9e38c60721218fec9823eb996438b0
B. Piatek <piatekbart@gmail.com>
2020-07-10 14:59:49 +0200

    first commit

---
+++ src/main/java/com/github/bpiatek/bbghbackend/BbghBackendApplication.java
@@ -0,0 +1,12 @@
+package com.github.bpiatek.bbghbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbghBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BbghBackendApplication.class, args);
+  }
+}
---
+++ src/test/java/com/github/bpiatek/bbghbackend/BbghBackendApplicationTests.java
@@ -0,0 +1,12 @@
+package com.github.bpiatek.bbghbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbghBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
