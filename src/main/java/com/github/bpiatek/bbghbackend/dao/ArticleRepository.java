c9c21164c99edd1fd72d0b555708a5d3104382a2
Bartosz Piatek <42154043+bpiatek@users.noreply.github.com>
2020-07-16 13:56:41 +0200

    save comment date added, add date_added column plus some tests (#11)
    Co-authored-by: B. Piatek <piatekbart@gmail.com>

--- src/main/java/com/github/bpiatek/bbghbackend/dao/ArticleRepository.java
+++ src/main/java/com/github/bpiatek/bbghbackend/dao/ArticleRepository.java
@@ -5,10 +5,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.Repository;
 
+import java.util.Optional;
+
 /**
  * Created by Bartosz Piatek on 10/07/2020
  */
 public interface ArticleRepository extends Repository<Article, Long> {
   Article save(Article article);
   Page<Article> findAll(Pageable pageable);
+  Optional<Article> findById(Long id);
 }
