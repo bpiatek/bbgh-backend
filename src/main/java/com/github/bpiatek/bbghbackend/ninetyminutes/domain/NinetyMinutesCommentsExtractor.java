1806c9509c51936017ac1fe36b25157cbb7ae71d
Bartosz Piatek <42154043+bpiatek@users.noreply.github.com>
2020-07-17 09:16:42 +0200

    a little bit of refactoring (#17)
    Co-authored-by: B. Piatek <piatekbart@gmail.com>

--- src/main/java/com/github/bpiatek/bbghbackend/ninetyminutes/domain/NinetyMinutesCommentsExtractor.java
+++ src/main/java/com/github/bpiatek/bbghbackend/ninetyminutes/domain/NinetyMinutesCommentsExtractor.java
@@ -1,17 +1,10 @@
 package com.github.bpiatek.bbghbackend.ninetyminutes.domain;
 
-import static org.apache.commons.text.StringEscapeUtils.unescapeHtml4;
-import static org.jsoup.Jsoup.parse;
-
 import com.github.bpiatek.bbghbackend.model.Comment;
 import com.github.bpiatek.bbghbackend.model.CommentHtmlExtractor;
 import lombok.extern.slf4j.Slf4j;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -24,48 +17,29 @@ import java.util.regex.Pattern;
 @Service
 class NinetyMinutesCommentsExtractor implements CommentHtmlExtractor {
 
-  private final TextToLocalDateTimeParser localDateTimeParser;
-
   private static final Pattern PATTERN = Pattern.compile("<a class=\"main\">(?<author>[^<]*)</a> - (?<date>.*?) - (?<authorHost>.*?)<br>\n(?<content>.*?)\n<p", Pattern.DOTALL);
   private static final int PATTERN_GROUPS_COUNT = 4;
 
-  NinetyMinutesCommentsExtractor(TextToLocalDateTimeParser localDateTimeParser) {
-    this.localDateTimeParser = localDateTimeParser;
+  private final CommentCreator commentCreator;
+
+  NinetyMinutesCommentsExtractor(CommentCreator commentCreator) {
+    this.commentCreator = commentCreator;
   }
 
   @Override
   public List<Comment> getComments(String html) {
-    Matcher matcher = PATTERN.matcher(html);
     List<Comment> comments = new ArrayList<>();
+    Matcher matcher = PATTERN.matcher(html);
 
     while (matcher.find()) {
       if (matcher.groupCount() == PATTERN_GROUPS_COUNT) {
+        Comment comment = commentCreator.createFromHtml(matcher);
 
-        Comment comment = Comment.builder()
-            .author(matcher.group("author"))
-            .content(matcher.group("content"))
-            .dateAdded(localDateTimeParser.parse(matcher.group("date")))
-            .build();
-
-        if (!containsComment(comments, comment)) {
+        if (!comments.contains(comment)) {
           comments.add(comment);
         }
       }
     }
-
     return comments;
-  }
-
-  private boolean containsComment(List<Comment> comments, Comment comment) {
-    for(Comment o : comments) {
-      if (
-          o.getAuthor().equals(comment.getAuthor()) &&
-          o.getContent().equals(comment.getContent()) &&
-          o.getDateAdded().equals(comment.getDateAdded())
-      ) {
-        return true;
-      }
-    }
-    return false;
   }
 }
