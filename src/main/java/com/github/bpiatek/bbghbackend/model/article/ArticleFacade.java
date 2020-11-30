5b93d19e01b61599ff9b6b1b19144f9b6b74da41
B. Piatek <piatekbart@gmail.com>
2020-11-30 20:21:50 +0100

    days limit in application.properties

--- src/main/java/com/github/bpiatek/bbghbackend/model/article/ArticleFacade.java
+++ src/main/java/com/github/bpiatek/bbghbackend/model/article/ArticleFacade.java
@@ -1,6 +1,7 @@
 package com.github.bpiatek.bbghbackend.model.article;
 
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -13,11 +14,19 @@ import java.util.List;
  * Created by Bartosz Piatek on 26/07/2020
  */
 @Service
-@AllArgsConstructor
 public class ArticleFacade {
 
   private final Clock clock;
   private final ArticleRepository articleRepository;
+  private Integer daysBack;
+
+  public ArticleFacade(ArticleRepository articleRepository,
+                       @Value("${article.daysback}")Integer daysBack,
+                       Clock clock) {
+    this.articleRepository = articleRepository;
+    this.daysBack = daysBack;
+    this.clock = clock;
+  }
 
   public Article findById(Long id) {
     return articleRepository.findById(id).orElseThrow(() -> new ArticleNotFoundException(id));
@@ -35,8 +44,8 @@ public class ArticleFacade {
     return articleRepository.save(article);
   }
 
-  public List<Article> findArticlesMaxThreeDaysOld() {
-    final LocalDateTime threeDaysAgo = LocalDateTime.now(clock).minusDays(3);
+  public List<Article> findArticlesNDaysOld() {
+    final LocalDateTime threeDaysAgo = LocalDateTime.now(clock).minusDays(daysBack);
     return articleRepository.findAllWithDateAfter(threeDaysAgo);
   }
 }
--- src/main/java/com/github/bpiatek/bbghbackend/ninetyminutes/domain/NinetyMinutesCrawler.java
+++ src/main/java/com/github/bpiatek/bbghbackend/ninetyminutes/domain/NinetyMinutesCrawler.java
@@ -73,7 +73,7 @@ class NinetyMinutesCrawler extends WebCrawler {
       return true;
     }
 
-    return articleFacade.findArticlesMaxThreeDaysOld()
+    return articleFacade.findArticlesNDaysOld()
         .stream()
         .anyMatch(article -> article.getUrl().equalsIgnoreCase(url));
   }
